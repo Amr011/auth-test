40c0d403bfea709760379e6f31e76a2a401881d3
Amr109 <grand.109.master@gmail.com>
2022-05-29 04:01:44 +0300

    first commit

---
+++ partials/footer.php
@@ -0,0 +1,4 @@
+<footer style="margin-top:50px; padding:0px;">
+    <p style="padding:0px;">copytight © 2022 </p>
+  <a style=" align-items:center; margin:0px; padding:0px; color:blue; text-decoration:underline;" href="amraboras.webflow.io">Amr Aboras</a>
+</footer>
